97846f77d2ae6afb54d0ea772f3dc07aa91ad6e8
RoGitaccount <162996846+RoGitaccount@users.noreply.github.com>
2024-03-18 15:24:09 +0100

    Add files via upload

---
+++ 11-porte foliot pers/navbar.php
@@ -0,0 +1,14 @@
+
+<header>
+        <h1>Mon Portfolio</h1>
+        <nav>
+            <ul>
+                <li><a href="menu.php">À propos</a></li>
+                <li><a href="./images/CV.html">CV</a></li>
+                <li><a href="formation.php">Formation</a></li>
+                <li><a href="Veille.php">Veille Techonologique</a></li>
+                <li><a href="conclusion.php">Conclusion</a></li>
+
+            </ul>
+        </nav>
+</header>
---
+++ 11-porte foliot pers/theme.js
@@ -0,0 +1,5 @@
+const themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
